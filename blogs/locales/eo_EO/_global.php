f9600822ea0939a0278022251a4d904c413c074d
Keith Bowes <zooplah@gmail.com>
2014-07-11 23:28:51 -0400

    In "Filter List", "Filter" is a verb, not a noun

--- blogs/locales/eo_EO/_global.php
+++ blogs/locales/eo_EO/_global.php
@@ -7,7 +7,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 $trans['eo_EO'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
-'' => "Project-Id-Version: b2evolution\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2014-07-11 00:08-0400\nPO-Revision-Date: 2014-07-11 00:11-0400\nLast-Translator: Keith Bowes <zooplah@gmail.com>\nLanguage-Team: Esperanto (http://www.transifex.com/projects/p/b2evolution/language/eo/)\nLanguage: eo\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'' => "Project-Id-Version: b2evolution\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2014-07-11 00:08-0400\nPO-Revision-Date: 2014-07-11 19:35-0400\nLast-Translator: Keith Bowes <zooplah@gmail.com>\nLanguage-Team: Esperanto (http://www.transifex.com/projects/p/b2evolution/language/eo/)\nLanguage: eo\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 'Categories' => 'Kategorioj',
 'Tags' => 'Etikedoj',
 'Previous' => 'Antaŭa',
@@ -604,7 +604,7 @@ $trans['eo_EO'] = array(
 'No results.' => 'Neniuj rezultoj.',
 'Apply' => 'Apliki',
 'Columns' => 'Kolumnoj',
-'Filter list' => 'Listo de filtriloj',
+'Filter list' => 'Filtri la liston',
 'Filters' => 'Filtriloj',
 'Change Order' => 'Ŝanĝi ordon',
 'months' => 'monatoj',
