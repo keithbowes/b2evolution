cb721b6dd07800396a2f309c2b672dc761ef4a5d
Keith Bowes <zooplah@gmail.com>
2014-12-07 21:44:03 -0500

    No hyphen in the button label

--- blogs/skins/edk/locales/de_DE/_global.php
+++ blogs/skins/edk/locales/de_DE/_global.php
@@ -7,7 +7,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 $trans['de_DE'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
-'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2013-12-23 03:15-0500\nPO-Revision-Date: 2014-07-27 17:42-0400\nLast-Translator: Keith <keith@localhost>\nLanguage-Team: German\nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2013-12-23 03:15-0500\nPO-Revision-Date: 2014-12-07 21:41-0500\nLast-Translator: Keith <keith@localhost>\nLanguage-Team: German\nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 '403 Forbidden' => '403 Verboten',
 '404 Not Found' => '404 Nicht Gefunden',
 '<p><a href="%s">%s</a> cannot resolve the requested URL.</p>' => '<p><a href="%s">%s</a> kann nicht die angeforderter URL auflösen.</p>',
@@ -19,7 +19,7 @@ $trans['de_DE'] = array(
 ' at ' => ' an ',
 '<a href="http://en.wikipedia.org/wiki/Markdown">Markdown</a> is enabled.' => '<a href="http://de.wikipedia.org/wiki/Markdown">Markdown</a> ist aktiviert.',
 '<a href="http://en.wikipedia.org/wiki/BBCode">BBCode</a> is enabled.' => '<a href="http://de.wikipedia.org/wiki/BBCode">BBCode</a> ist aktiviert.',
-'%s <span class="button-sf">Feed</span>' => '%s-<span class="button-sf">Feed</span>',
+'%s <span class="button-sf">Feed</span>' => '%s<span class="button-sf">-Feed</span>',
 '%s Feed' => '%s-Feed',
 'Valid XHTML' => 'Gültige XHTML',
 'Share on Facebook' => 'Teilen auf Facebook',
