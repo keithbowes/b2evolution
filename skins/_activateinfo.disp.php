0702c3e38660d45999cc1d243c7905168ef44b54
Keith Bowes <zooplah@gmail.com>
2015-03-08 14:47:17 -0400

    Restored my modified files

--- skins/_activateinfo.disp.php
+++ skins/_activateinfo.disp.php
@@ -4,11 +4,14 @@
  *
  * This file is not meant to be called directly.
  *
- * @license GNU GPL v2 - {@link http://b2evolution.net/about/gnu-gpl-license}
- *
- * @copyright (c)2003-2015 by Francois Planque - {@link http://fplanque.com/}.
+ * @copyright (c)2003-2014 by Francois Planque - {@link http://fplanque.com/}.
  *
  * @package evocore
+ *
+ * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
+ * @author asimo: Evo Factory / Attila Simo
+ *
+ * @version $Id$
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
@@ -20,43 +23,17 @@ if( empty( $redirect_to ) )
 	$redirect_to = regenerate_url( 'disp' );
 }
 
-// Default params:
-$params = array_merge( array(
-		'skin_form_before'     => '',
-		'skin_form_after'      => '',
-		'activate_form_title'  => '',
-		'activate_page_before' => '',
-		'activate_page_after'  => '',
-		'activate_form_params' => NULL,
-		'use_form_wrapper'     => true,
-	), $params );
-
 $display_params = array(
-	'use_form_wrapper' => $params['use_form_wrapper'],
-	'form_before'      => str_replace( '$form_title$', $params['activate_form_title'], $params['skin_form_before'] ),
-	'form_after'       => $params['skin_form_after'],
-	'form_action'      => $secure_htsrv_url.'login.php',
-	'form_name'        => 'activateinfo_form',
-	'form_class'       => 'bComment',
-	'form_layout'      => NULL,
-	'redirect_to'      => url_rel_to_same_host( $redirect_to, $secure_htsrv_url ),
-	'inskin'           => true,
-	'blog'             => ( ( isset( $blog ) ) ? $blog : NULL ),
-	'form_template'    => $params['activate_form_params'],
+	'form_action' => $secure_htsrv_url.'login.php',
+	'form_name' => 'activateinfo_form',
+	'form_class' => 'bComment',
+	'form_layout' => NULL,
+	'redirect_to' => url_rel_to_same_host( $redirect_to, $secure_htsrv_url ),
+	'inskin' => true,
+	'blog' => ( ( isset( $blog ) ) ? $blog : NULL )
 );
-
-echo $params['activate_page_before'];
-
-if( $params['display_form_messages'] )
-{ // Display the form messages before form inside wrapper
-	messages( array(
-			'block_start' => '<div class="action_messages">',
-			'block_end'   => '</div>',
-		) );
-}
 
 // display account activate info
 display_activateinfo( $display_params );
 
-echo $params['activate_page_after'];
 ?>
