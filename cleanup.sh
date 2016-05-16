c11a36a88970aeebdde7baa9b265a4f04e083c97
Keith Bowes <zooplah@gmail.com>
2016-05-16 10:10:47 -0400

    Remove files from version control

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,13 @@ echo Removing files from merges
 find . -name '*~' -delete
 find . -name '*.new' -delete
 find . -name '*.orig' -delete
+
+echo Removing files from version control
+rm -fr `find . -name 'CVS'`
+rm -fr `find . -name '.cvs*'`
+rm -fr `find . -name '.git*'`
+rm -fr `find . -name '.svn*'`
+
+echo Removing additional files
+rm -f TODO
+rm -f pods.txt
