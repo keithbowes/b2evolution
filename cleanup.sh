090fe2e8d0a2dcba6ce4d90db813a25d5821b9a6
Keith Bowes <keithbowes@users.noreply.github.com>
2016-06-28 19:38:46 -0400

    Remove the .tx directory before uploading too

--- cleanup.sh
+++ cleanup.sh
@@ -9,6 +9,9 @@ rm -fr `find . -name '.cvs*'`
 rm -fr `find . -name '.git*'`
 rm -fr `find . -name '.svn*'`
 
+echo Removing files from Transifex
+rm -fr .tx
+
 echo Removing additional files
 rm -f TODO
 rm -f pods.txt
