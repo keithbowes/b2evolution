ee502610ff317a05be30c9326c38ecee8441f94a
Keith Bowes <keithbowes@users.noreply.github.com>
2016-07-08 20:02:19 -0400

    Moved the locales and skins to separate repositories

--- skins/_esf/ad.include.php
+++
@@ -1,8 +0,0 @@
-
-#
-# <?php echo $Skin->T_("More info about ESF feeds:\n");?>
-# http://www.aquarionics.com/article/name/esf
-#
-# <?php echo $Skin->T_("Self-promotion: To read ESF feeds, you can download my feed\n# reader:\n"); ?>
-# http://github.com/keithbowes/r3r
-#
