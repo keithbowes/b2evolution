d8316aa6061632a52c05fbbff1372df48c8b1e66
Keith Bowes <keithbowes@users.noreply.github.com>
2018-06-12 12:58:31 -0400

    Removed bower and grunt files

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,7 @@
 echo Removing unnecessary files from distribution
 rm -rf _tests
 rm -rf _transifex
-rm -f Gruntfile.js
-rm -f package.json
 rm -f readme.html
-rm -f .bower.json
 rm -f .gitmodules
 
 echo Removing test skins
